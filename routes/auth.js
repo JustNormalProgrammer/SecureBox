ed01daca521dc65733907c057de3bd88790bd24c
JustNormalProgrammer <testkonto1290@gmail.com>
2025-04-01 17:42:41 +0200

    Implement drizzly in express routes and middleware

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
-const User = require('../models/user');
+const asyncHandler = require("express-async-handler");
+const CustomError = require("../utils/customError");
+const {getUserByLoginAndPassword} = require('../config/db/queries/users');
 const { SECRET_KEY, TOKEN_EXPIRATION_MINUTES } = require('../middleware/auth');
 
-router.post('/', (req, res) => {
+router.post('/', asyncHandler( async (req, res) => {
   const { login, password } = req.body;
-  User.findByLoginAndPassword(login, password, (err, user) => {
-    if (err || !user) return res.status(401).json({ detail: 'Invalid login or password' });
+  try{
+    const [user] = await getUserByLoginAndPassword(login, password);
+    if(!user)
+      throw new CustomError('Invalid login or password', 401)
     const token = jwt.sign(
       { user_id: user.id },
       SECRET_KEY,
@@ -17,7 +21,9 @@ router.post('/', (req, res) => {
       user: { id: user.id, first_name: user.first_name, last_name: user.last_name, login: user.login },
       token,
     });
-  });
-});
+  } catch(err){
+    return res.status(err.status || 500).json({ detail: err.message });
+  }
+}));
 
 module.exports = router;
